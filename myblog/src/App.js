bd6ac9f4cc841e393ca81dbf6d420daa9f4ce484
Fabian295 <fabiankanhai70@gmail.com>
2022-12-04 20:31:11 +0100

    Add frontend, react app

---
+++ myblog/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.png';
+// import { useEffect, useState } from 'react';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [blogs, setBlogs] = useState([])
+  // const url = 'https://jsonplaceholder.typicode.com/posts';
+  const url2 = 'http://localhost:4000/'
+
+  useEffect( () => {
+    // fetch(url)
+    fetch(url2)
+      .then( res =>  res.json())
+      .then( data => setBlogs(data))
+      // console.log(blogs)
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <h1 className="blog">FK Blog</h1>
+        <ul className="nav">
+          <li><a href="/" className="nav-link">Home</a></li>
+          <li><a href="/" className="nav-link">Blog</a></li>
+          <li><a href="/" className="nav-link">Projects</a></li>
+        </ul>
+      </header>
+
+      <main className="blog-list">
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.id}>{blog.title}</li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
